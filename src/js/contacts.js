bce4bb42f659afa14e9fb9b5b0625b559ec485c8
David Peterka <143167841+DavidPeterK@users.noreply.github.com>
2023-12-21 04:14:29 +0100

    save

--- src/js/contacts.js
+++ src/js/contacts.js
@@ -9,3 +9,23 @@ function showContactsPopUp() {
     let popup = document.getElementById('contactsPopUp');
     popup.classList.remove('d-none');
 }
+
+/** * This function is to save the input in the contact array */
+async function createContact() {
+    let newContact = {
+        "name": document.getElementById('contactUserName').value,
+        "nameAbbreviation": makeNameAbbreviation(document.getElementById('contactUserName').value),
+        "email": document.getElementById('contactEmail').value,
+        "phone": document.getElementById('inputPhoneId').value,
+        "color": getColor()
+    }
+    contactsArray.push(newContact);
+    await currentUserContactsSave();
+    clearInputFields();
+    slideOut('swipeContactPopupId', 'addContactId', 200);
+    toggleVisibility('mobileBackArrowId', false);
+    toggleVisibility('mobileVisibilityId', true);
+    renderContacts();
+    changesSaved('Contact successfully created');
+    hoverNewContact(newContact);
+}
