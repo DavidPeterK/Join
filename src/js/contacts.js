1ac565114f5fb78d3bad9ae9500ee286365582ae
David Peterka <143167841+DavidPeterK@users.noreply.github.com>
2023-12-21 14:14:04 +0100

    save

--- src/js/contacts.js
+++ src/js/contacts.js
@@ -16,16 +16,67 @@ async function createContact() {
         "name": document.getElementById('contactUserName').value,
         "nameAbbreviation": makeNameAbbreviation(document.getElementById('contactUserName').value),
         "email": document.getElementById('contactEmail').value,
-        "phone": document.getElementById('inputPhoneId').value,
+        "phone": document.getElementById('contactPhone').value,
         "color": getColor()
     }
     contactsArray.push(newContact);
     await currentUserContactsSave();
     clearInputFields();
-    slideOut('swipeContactPopupId', 'addContactId', 200);
-    toggleVisibility('mobileBackArrowId', false);
-    toggleVisibility('mobileVisibilityId', true);
     renderContacts();
     changesSaved('Contact successfully created');
-    hoverNewContact(newContact);
 }
+
+/**
+ * Validates the input value of a form's phone field.
+ * Checks whether the entered phone number only contains the plus symbol and digits 0-9. 
+ * If the validation fails, it displays an error message and prevents form submission. 
+ * Otherwise, it allows form submission.
+ */
+function validateForm() {
+    var input = document.getElementById('inputPhoneId');
+
+    var regex = /^[+0-9]+$/;
+
+    if (!regex.test(input.value)) {
+        input.style.borderColor = 'red';
+        document.getElementById('errorMessage').innerText = "Invalid input! Only + and numbers from 0-9 are allowed.";
+        setTimeout(function () {
+            input.style.borderColor = '#A8A8A8';
+            document.getElementById('errorMessage').innerText = "";
+        }, 6000);
+        return false; // Verhindert das Absenden des Formulars
+    } else {
+        document.getElementById('errorMessage').innerText = "";
+        return true; // Ermöglicht das Absenden des Formulars
+    }
+}
+
+/**
+ * Creates a 2-letter abbreviation from a given name (e.g. "John Doe" -> "JD").
+ */
+function makeNameAbbreviation(name) {
+    let nameParts = name.split(' ');
+    let firstName = nameParts[0];
+    let lastName = nameParts.length > 1 ? nameParts[nameParts.length - 1] : '';
+    let nameAbbreviation = `${firstName.charAt(0).toUpperCase()}${lastName.charAt(0).toUpperCase()}`;
+    return nameAbbreviation;
+}
+
+/** * This function is to clear the input fields in a popup */
+function clearInputFields() {
+    document.getElementById('inputNameId').value = '';
+    document.getElementById('inputEmailId').value = '';
+    document.getElementById('inputPhoneId').value = '';
+}
+
+/** * This function is used to create the profile image color */
+function getColor() {
+    if (nextColorIndex >= colorArray.length) {
+        nextColorIndex = 0;
+    }
+    let color = colorArray[nextColorIndex];
+    nextColorIndex++;
+    setItem('nextColorIndex', JSON.stringify(nextColorIndex));
+    return color;
+}
+
